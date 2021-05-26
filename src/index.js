d544efc15d87236f4ebd5b42deb92ee70101df18
stacksagar <secure.js.info@gmail.com>
2021-05-26 21:04:00 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -9,6 +9,37 @@ import { store, persistor } from './Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 
 
+// import { ApolloProvider } from 'react-apollo';
+import { ApolloClient, gql } from 'apollo-boost';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'https://crwn-clothing.com',
+});
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+client.query({
+  query: gql`
+   {
+    collection(id:"cjwuuj5bz000i0719rrtw5gqk")  {
+      title
+      id
+      items {
+        id 
+        name
+      }
+    }
+   }
+  `
+}).then(console.log)
+
+
 ReactDOM.render(
   <React.StrictMode> 
       <Provider store={store}>
